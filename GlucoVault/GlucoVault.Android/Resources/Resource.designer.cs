129f085e3640c1c0c6706cbcc9a56fbb287c675f
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2021-03-04 20:40:13 +0200

    Changed the Tab icons

--- GlucoVault/GlucoVault.Android/Resources/Resource.designer.cs
+++ GlucoVault/GlucoVault.Android/Resources/Resource.designer.cs
@@ -8310,49 +8310,49 @@ namespace GlucoVault.Droid
 			public const int avd_show_password = 2131165275;
 			
 			// aapt resource value: 0x7F07005C
-			public const int bear = 2131165276;
+			public const int cooking = 2131165276;
 			
 			// aapt resource value: 0x7F07005D
-			public const int cat = 2131165277;
+			public const int design_bottom_navigation_item_background = 2131165277;
 			
 			// aapt resource value: 0x7F07005E
-			public const int design_bottom_navigation_item_background = 2131165278;
+			public const int design_fab_background = 2131165278;
 			
 			// aapt resource value: 0x7F07005F
-			public const int design_fab_background = 2131165279;
+			public const int design_ic_visibility = 2131165279;
 			
 			// aapt resource value: 0x7F070060
-			public const int design_ic_visibility = 2131165280;
+			public const int design_ic_visibility_off = 2131165280;
 			
 			// aapt resource value: 0x7F070061
-			public const int design_ic_visibility_off = 2131165281;
+			public const int design_password_eye = 2131165281;
 			
 			// aapt resource value: 0x7F070062
-			public const int design_password_eye = 2131165282;
+			public const int design_snackbar_background = 2131165282;
 			
 			// aapt resource value: 0x7F070063
-			public const int design_snackbar_background = 2131165283;
+			public const int dinner = 2131165283;
 			
 			// aapt resource value: 0x7F070064
-			public const int dog = 2131165284;
+			public const int glucose = 2131165284;
 			
 			// aapt resource value: 0x7F070065
-			public const int elephant = 2131165285;
+			public const int ic_mtrl_chip_checked_black = 2131165285;
 			
 			// aapt resource value: 0x7F070066
-			public const int ic_mtrl_chip_checked_black = 2131165286;
+			public const int ic_mtrl_chip_checked_circle = 2131165286;
 			
 			// aapt resource value: 0x7F070067
-			public const int ic_mtrl_chip_checked_circle = 2131165287;
+			public const int ic_mtrl_chip_close_circle = 2131165287;
 			
 			// aapt resource value: 0x7F070068
-			public const int ic_mtrl_chip_close_circle = 2131165288;
+			public const int info = 2131165288;
 			
 			// aapt resource value: 0x7F070069
-			public const int info = 2131165289;
+			public const int list = 2131165289;
 			
 			// aapt resource value: 0x7F07006A
-			public const int monkey = 2131165290;
+			public const int meter = 2131165290;
 			
 			// aapt resource value: 0x7F07006B
 			public const int mtrl_snackbar_background = 2131165291;
@@ -8400,10 +8400,10 @@ namespace GlucoVault.Droid
 			public const int notify_panel_notification_icon_bg = 2131165305;
 			
 			// aapt resource value: 0x7F07007A
-			public const int paw = 2131165306;
+			public const int photo = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int photo = 2131165307;
+			public const int recipe = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
 			public const int splash_logo = 2131165308;
@@ -8418,7 +8418,10 @@ namespace GlucoVault.Droid
 			public const int tooltip_frame_light = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int vault = 2131165312;
+			public const int update = 2131165312;
+			
+			// aapt resource value: 0x7F070081
+			public const int vault = 2131165313;
 			
 			static Drawable()
 			{
