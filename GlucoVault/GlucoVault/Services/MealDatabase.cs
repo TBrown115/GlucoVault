87d2959c755a32d06acc5c58969d4f60662c8544
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2021-03-24 08:12:18 +0200

    Update Meal Planner

--- GlucoVault/GlucoVault/Services/MealDatabase.cs
+++ GlucoVault/GlucoVault/Services/MealDatabase.cs
@@ -42,18 +42,21 @@ namespace GlucoVault.Services
             {
                 var dailymealplanner = new DailyMealPlan
                 {
+                    Id = 1,
                     RecipeId = 1,
                 };
                  _database.Insert(dailymealplanner);
 
                 var dietrequirement = new DietRequirement
                 {
+                    Id = 1,
                     Description = "Normal"
                 };
                 _database.Insert(dietrequirement);
 
                 var dietaryoption = new DietaryOption
                 {
+                    Id = 1,
                     DietaryRequirements = 1,
                     BudgetOption = 250
                 };
@@ -61,7 +64,38 @@ namespace GlucoVault.Services
 
                 var ingredient = new Ingredient
                 {
+                    Id = 1,
                     Description = "Strawberry, Banana, Orange Juice",
+                    QuantityDescription = "1 cup",
+                    Volume = 152,
+                    VolumeUnit = "g",
+                    Image = "vault.png"
+
+                };
+                ingredient = new Ingredient
+                {
+                    Id = 1,
+                    Description = "Strawberry",
+                    QuantityDescription = "1 cup",
+                    Volume = 152,
+                    VolumeUnit = "g",
+                    Image = "vault.png"
+
+                };
+                ingredient = new Ingredient
+                {
+                    Id = 2,
+                    Description = "Banana",
+                    QuantityDescription = "1 medium",
+                    Volume = 152,
+                    VolumeUnit = "g",
+                    Image = "vault.png"
+
+                };
+                ingredient = new Ingredient
+                {
+                    Id = 3,
+                    Description = "Orange Juice",
                     QuantityDescription = "1 cup",
                     Volume = 152,
                     VolumeUnit = "g",
@@ -72,6 +106,7 @@ namespace GlucoVault.Services
 
                 var mealtype = new MealType
                 {
+                    Id = 1,
                     Description = "Breakfast"
 
                 };
@@ -79,6 +114,7 @@ namespace GlucoVault.Services
 
                 var recipe = new Recipe
                 {
+                    Id = 1,
                     Description = "Breakfast fruit smoothie",
                     Prep = 7,
                     CookTime = 0,
@@ -92,58 +128,168 @@ namespace GlucoVault.Services
                     DietRequirementId = 1
                 };
                 _database.Insert(recipe);
-                _database.Insert(recipe);
 
                 var recipeingredient = new RecipeIngredient
                 {
                     RecipeId = 2,
                     IngredientId = 1
                 };
-                
+                recipeingredient = new RecipeIngredient
+                {
+                    RecipeId = 2,
+                    IngredientId = 2
+                };
+                recipeingredient = new RecipeIngredient
+                {
+                    RecipeId = 2,
+                    IngredientId = 3
+                };
+
                 _database.Insert(recipeingredient);
                 _database.UpdateWithChildren(recipe);
-      
-            }
-            if (_database.Table<Models.DailyMealPlan>().Count() == 0)
-            {
-                var dailymealplanner = new DailyMealPlan
+
+                //Apple
+                dailymealplanner = new DailyMealPlan
                 {
-                    RecipeId = 1,
+                    Id = 2,
+                    RecipeId = 2,
                 };
                 _database.Insert(dailymealplanner);
 
-                var dietrequirement = new DietRequirement
+                dietrequirement = new DietRequirement
                 {
+                    Id = 1,
                     Description = "Normal"
                 };
                 _database.Insert(dietrequirement);
 
-                var dietaryoption = new DietaryOption
+                new DietaryOption
                 {
+                    Id = 1,
                     DietaryRequirements = 1,
                     BudgetOption = 250
                 };
                 _database.Insert(dietaryoption);
 
-                var ingredient = new Ingredient
+                ingredient = new Ingredient
                 {
-                    Description = "Olive oil, Feta cheese,Spinach(Raw),Egg",
+                    Id = 4,
+                    Description = "Apple",
+                    QuantityDescription = "1 Apple",
+                    Volume = 129,
+                    VolumeUnit = "g",
+                    Image = "https://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg"
+
+                };
+                
+                _database.Insert(ingredient);
+
+                mealtype = new MealType
+                {
+                    Id = 1,
+                    Description = "Breakfast"
+
+                };
+                _database.Insert(mealtype);
+
+                recipe = new Recipe
+                {
+                    Description = "Pick a Apple",
+                    Prep = 0,
+                    CookTime = 0,
+                    Directions = "Wilt spinach down in a small skillet over low heat with 1/2 teaspoon of olive oil. Season to taste.Beat the eggs and add to the skillet with the spinach.Stir slowly over medium - low heat until they reach your desired doneness.Sprinkle in the feta cheese and stir to combine and soften the cheese.",
+                    Volume = 7,
+                    Price = 10,
+                    Image = "https://upload.wikimedia.org/wikipedia/commons/c/cf/Scrambled_Eggs_at_Cracker_Barrel.jpg",
+                    ServingSize = "1 Apple",
+                    Calories = 94,
+                    MealType = 1,
+                    DietRequirementId = 1
+                };
+                _database.Insert(recipe);
+                _database.UpdateWithChildren(recipe);
+
+                recipeingredient = new RecipeIngredient
+                {
+                    RecipeId = 3,
+                    IngredientId = 9,
+                };
+                
+                _database.Insert(recipeingredient);
+
+                dailymealplanner = new DailyMealPlan
+                {
+                    Id = 2,
+                    RecipeId = 2,
+                };
+                _database.Insert(dailymealplanner);
+
+                dietrequirement = new DietRequirement
+                {
+                    Id = 1,
+                    Description = "Normal"
+                };
+                _database.Insert(dietrequirement);
+
+                new DietaryOption
+                {
+                    Id = 1,
+                    DietaryRequirements = 1,
+                    BudgetOption = 250
+                };
+                _database.Insert(dietaryoption);
+
+                ingredient = new Ingredient
+                {
+                    Id = 4,
+                    Description = "Olive oil",
                     QuantityDescription = "1 cup",
-                    Volume = 152,
+                    Volume = 2,
+                    VolumeUnit = "g",
+                    Image = "https://upload.wikimedia.org/wikipedia/commons/c/cf/Scrambled_Eggs_at_Cracker_Barrel.jpg"
+
+                };
+                ingredient = new Ingredient
+                {
+                    Id = 5,
+                    Description = "Feta cheese",
+                    QuantityDescription = "0.13 cup crumbled",
+                    Volume = 18,
+                    VolumeUnit = "g",
+                    Image = "https://upload.wikimedia.org/wikipedia/commons/c/cf/Scrambled_Eggs_at_Cracker_Barrel.jpg"
+
+                };
+                ingredient = new Ingredient
+                {
+                    Id = 6,
+                    Description = "Spinach(Raw)",
+                    QuantityDescription = "1 cup",
+                    Volume = 30,
+                    VolumeUnit = "g",
+                    Image = "https://upload.wikimedia.org/wikipedia/commons/c/cf/Scrambled_Eggs_at_Cracker_Barrel.jpg"
+
+                };
+                ingredient = new Ingredient
+                {
+                    Id = 7,
+                    Description = "Egg",
+                    QuantityDescription = "2 large",
+                    Volume = 100,
                     VolumeUnit = "g",
                     Image = "https://upload.wikimedia.org/wikipedia/commons/c/cf/Scrambled_Eggs_at_Cracker_Barrel.jpg"
 
                 };
                 _database.Insert(ingredient);
 
-                var mealtype = new MealType
+                mealtype = new MealType
                 {
+                    Id = 1,
                     Description = "Breakfast"
 
                 };
                 _database.Insert(mealtype);
 
-                var recipe = new Recipe
+                recipe = new Recipe
                 {
                     Description = "Scrambled Eggs with Spinach and Feta",
                     Prep = 5,
@@ -151,7 +297,7 @@ namespace GlucoVault.Services
                     Directions = "Wilt spinach down in a small skillet over low heat with 1/2 teaspoon of olive oil. Season to taste.Beat the eggs and add to the skillet with the spinach.Stir slowly over medium - low heat until they reach your desired doneness.Sprinkle in the feta cheese and stir to combine and soften the cheese.",
                     Volume = 6,
                     Price = 25,
-                    Image = "https://upload.wikimedia.org/wikipedia/commons/7/7e/Strawberry_BNC.jpg",
+                    Image = "https://upload.wikimedia.org/wikipedia/commons/c/cf/Scrambled_Eggs_at_Cracker_Barrel.jpg",
                     ServingSize = "1 services",
                     Calories = 280,
                     MealType = 1,
@@ -160,14 +306,34 @@ namespace GlucoVault.Services
                 _database.Insert(recipe);
                 _database.UpdateWithChildren(recipe);
 
-                var recipeingredient = new RecipeIngredient
+                recipeingredient = new RecipeIngredient
                 {
                     RecipeId = 1,
                     IngredientId = 9,
                 };
+                recipeingredient = new RecipeIngredient
+                {
+                    RecipeId = 1,
+                    IngredientId = 10,
+                };
+                recipeingredient = new RecipeIngredient
+                {
+                    RecipeId = 1,
+                    IngredientId = 11,
+                };
+                recipeingredient = new RecipeIngredient
+                {
+                    RecipeId = 1,
+                    IngredientId = 12,
+                };
                 _database.Insert(recipeingredient);
-                
+
             }
+            
+            
+                
+                
+            
         }
         public List<Recipe> GetDailyMealPlans()
         {
